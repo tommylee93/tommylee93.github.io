fb0c4901afb8e659df4c9a3df86214f53664130b
Domenico <domenicosottilotta93@gmail.com>
2025-03-19 23:17:31 +0100

    update telegram

--- telegram/main_bot.py
+++ telegram/main_bot.py
@@ -1,9 +1,9 @@
 from aiogram import Bot, Dispatcher, types
-from aiogram.types import WebAppInfo
+from aiogram.types import WebAppInfo, ReplyKeyboardMarkup, KeyboardButton
 from aiogram.utils import executor
 import logging
 
-TOKEN = "7499818544:AAEehBYH5cSLemi9jBz2DMuabdUuy_JjqJY"
+TOKEN = "INSERISCI_IL_TUO_TOKEN_BOT"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
@@ -12,10 +12,18 @@ logging.basicConfig(level=logging.INFO)
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    button = types.KeyboardButton(text="Apri WebApp", web_app=WebAppInfo(url="https://tommylee93.github.io"))
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    button = KeyboardButton(
+        text="Apri WebApp", 
+        web_app=WebAppInfo(url="https://tommylee93.github.io/")
+    )
     keyboard.add(button)
+    
     await message.answer("Clicca sul pulsante per aprire la WebApp:", reply_markup=keyboard)
+
+@dp.message_handler(content_types=types.ContentTypes.WEB_APP_DATA)
+async def webapp_data(message: types.Message):
+    await message.answer(f"Hai inviato: {message.web_app_data.data}")
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
