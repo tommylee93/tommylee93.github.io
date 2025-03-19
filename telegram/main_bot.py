cdc63e2f2f42d3002c2631ba0d7d2b9915e64d0a
Domenico <domenicosottilotta93@gmail.com>
2025-03-19 23:01:57 +0100

    update telegram

--- telegram/main_bot.py
+++ telegram/main_bot.py
@@ -3,7 +3,7 @@ from aiogram.types import WebAppInfo
 from aiogram.utils import executor
 import logging
 
-TOKEN = "INSERISCI_IL_TUO_TOKEN_BOT"
+TOKEN = "7499818544:AAEehBYH5cSLemi9jBz2DMuabdUuy_JjqJY"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
@@ -13,7 +13,7 @@ logging.basicConfig(level=logging.INFO)
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    button = types.KeyboardButton(text="Apri WebApp", web_app=WebAppInfo(url="https://tommylee93.github.io./"))
+    button = types.KeyboardButton(text="Apri WebApp", web_app=WebAppInfo(url="https://tommylee93.github.io"))
     keyboard.add(button)
     await message.answer("Clicca sul pulsante per aprire la WebApp:", reply_markup=keyboard)
 
